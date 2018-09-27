08440a667bcd60bd23472c5976ce99c9a77fc660
Matthew White <Matthew.White02@gmail.com>
2018-09-27 16:44:52 +0100

    Use a model to pass data to the About page

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFood.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,9 +16,11 @@ namespace OdeToFood.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var model = new AboutModel {
+                Name = "Matt",
+                Location = "Dublin, IE",
+            };
+            return View(model);
         }
 
         public ActionResult Contact()
