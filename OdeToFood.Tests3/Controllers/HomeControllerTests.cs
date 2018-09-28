b58aae094427c3412a6d1d54e132c5fd4ebd02fd
Unknown <Matthew.White02@gmail.com>
2018-09-28 14:23:37 +0100

    Add unit tests

---
+++ OdeToFood.Tests3/Controllers/HomeControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OdeToFood.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            HomeController controller = new HomeController();
+
+            ViewResult result = controller.About() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void AboutTest()
+        {
+            HomeController controller = new HomeController();
+
+            ViewResult result = controller.About() as ViewResult;
+
+            Assert.IsNotNull(result.Model);
+        }
+
+        [TestMethod()]
+        public void ContactTest()
+        {
+        }
+    }
+}
