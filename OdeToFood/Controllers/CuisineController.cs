da9336b53baad5cacf2cdc50fc92a5e4e5d2ac42
Unknown <Matthew.White02@gmail.com>
2018-09-28 15:22:51 +0100

    Return different ActionResult types

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -13,7 +13,12 @@ namespace OdeToFood.Controllers
         {
             var message = Server.HtmlEncode(name);
 
-            return Content(message);
+            //return Content(message);
+            //return RedirectPermanent("google.ie");
+            //return RedirectToAction("Index", "Home", new { name = name });
+            //return RedirectToRoute("Default", new { controller="Home", action="about" });
+            //return File(Server.MapPath("~/Content/Site.css"), "text/css");
+            return Json(new { message = message, name = "Matt" }, JsonRequestBehavior.AllowGet);
         }
     }
 }
