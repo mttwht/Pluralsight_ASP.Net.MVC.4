b705d2ad799869db46c8050ecf5833ae98b3fd0f
Unknown <Matthew.White02@gmail.com>
2018-09-28 16:09:39 +0100

    Add an attribute filter

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFood.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,18 +9,15 @@ namespace OdeToFood.Controllers
 {
     public class CuisineController : Controller
     {
-        [HttpPost]
+        //[Authorize]
+        [Log]
         public ActionResult Search(string name = "French")
         {
+            throw new Exception("OH NO!");
+
             var message = Server.HtmlEncode(name);
 
             return Content(message);
-        }
-
-        [HttpGet]
-        public ActionResult Search()
-        {
-            return Content("GET: Search!");
         }
     }
 }
