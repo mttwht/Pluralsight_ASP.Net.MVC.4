bf999675d5dcabd5ab8cb0e7fc2cc3e0c7774e67
Unknown <Matthew.White02@gmail.com>
2018-09-28 15:32:07 +0100

    Use Action Selectors

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -8,17 +8,23 @@ namespace OdeToFood.Controllers
 {
     public class CuisineController : Controller
     {
-        // GET: Cuisine
+        [HttpPost]
         public ActionResult Search(string name = "French")
         {
             var message = Server.HtmlEncode(name);
 
-            //return Content(message);
+            return Content(message);
             //return RedirectPermanent("google.ie");
             //return RedirectToAction("Index", "Home", new { name = name });
             //return RedirectToRoute("Default", new { controller="Home", action="about" });
             //return File(Server.MapPath("~/Content/Site.css"), "text/css");
-            return Json(new { message = message, name = "Matt" }, JsonRequestBehavior.AllowGet);
+            //return Json(new { message = message, name = "Matt" }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return Content("GET: Search!");
         }
     }
 }
