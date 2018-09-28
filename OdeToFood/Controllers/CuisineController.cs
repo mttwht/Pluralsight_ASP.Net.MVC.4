15085f01da879a55ee3d5831709c8786b03e6bb7
Unknown <Matthew.White02@gmail.com>
2018-09-28 15:32:52 +0100

    Remove commented code

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -14,11 +14,6 @@ namespace OdeToFood.Controllers
             var message = Server.HtmlEncode(name);
 
             return Content(message);
-            //return RedirectPermanent("google.ie");
-            //return RedirectToAction("Index", "Home", new { name = name });
-            //return RedirectToRoute("Default", new { controller="Home", action="about" });
-            //return File(Server.MapPath("~/Content/Site.css"), "text/css");
-            //return Json(new { message = message, name = "Matt" }, JsonRequestBehavior.AllowGet);
         }
 
         [HttpGet]
